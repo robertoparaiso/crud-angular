bc6ae178d03a935a41eb7915d72884fdf2543e14
robertoparaiso <robertoparaiso@yahoo.com.br>
2022-08-14 00:03:01 -0300

    primeiro commit

---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, first, take, tap } from 'rxjs';
+
+import { Course } from './../model/course';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  private readonly API = 'api/courses';
+
+  constructor(private httpClient: HttpClient) { }
+
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      //delay(5000),
+      tap(courses => console.log(courses))
+    );
+  }
+}
