4c1514f8c793d460a143ee1815796d7fca4934ff
robertoparaiso <robertoparaiso@yahoo.com.br>
2022-08-17 00:07:06 -0300

    criacao do formulario

---
+++ src/app/courses/course-form/course-form.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+import { CoursesService } from '../services/courses.service';
+
+@Component({
+  selector: 'app-course-form',
+  templateUrl: './course-form.component.html',
+  styleUrls: ['./course-form.component.scss']
+})
+export class CourseFormComponent implements OnInit {
+
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder,
+    private service: CoursesService,
+    private snackBar: MatSnackBar) {
+    this.form = this.formBuilder.group( {
+      name: [null],
+      category: [null]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    this.service.save(this.form.value)
+      .subscribe(e => console.log(e), error => this.onError());
+  }
+
+  onCancel() {
+
+  }
+
+  onError(){
+    this.snackBar.open('Erro ao salvar curso.', '', { duration: 3000 });
+  }
+
+}
