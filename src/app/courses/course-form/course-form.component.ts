a0d55ee5c1049e82fefd10f37e20d2f7b1d65c7f
robertoparaiso <robertoparaiso@yahoo.com.br>
2022-08-18 23:22:00 -0300

    manutencao na mensagem ao gravar

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -15,7 +16,8 @@ export class CourseFormComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder,
     private service: CoursesService,
-    private snackBar: MatSnackBar) {
+    private snackBar: MatSnackBar,
+    private location: Location) {
     this.form = this.formBuilder.group( {
       name: [null],
       category: [null]
@@ -27,14 +29,19 @@ export class CourseFormComponent implements OnInit {
 
   onSubmit() {
     this.service.save(this.form.value)
-      .subscribe(e => console.log(e), error => this.onError());
+      .subscribe(e => this.onSuccess(), error => this.onError());
   }
 
   onCancel() {
-
+    this.location.back();
   }
 
-  onError(){
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 3000 });
+    this.onCancel();
+  }
+
+  private onError() {
     this.snackBar.open('Erro ao salvar curso.', '', { duration: 3000 });
   }
 
